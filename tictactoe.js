c732d05d948a8fc2a92ce7544e01aee9e28b73ae
alanzhang03 <alan.s.zhang@gmail.com>
2024-05-28 17:43:01 -0400

    player objects

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,16 @@
-console.log("h");
+
+
+
+function startGame(){
+    
+}
+
+
+
+function player(name,marker){
+    this.name = name;
+
+}
+
+let player1 = new player("Alan", "O");
+let player2 = new player("Bob", "X");
