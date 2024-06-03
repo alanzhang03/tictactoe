12fb593761d319a69fed3117ceef30774d4922c9
alanzhang03 <alan.s.zhang@gmail.com>
2024-06-03 09:46:55 -0400

    buttons target all instances

--- tictactoe.js
+++ tictactoe.js
@@ -22,11 +22,29 @@ const selectButtonAi = document.querySelector(".select-button-ai");
 const selectButtonPlayer = document.querySelector(".select-button-player");
 const playerModal = document.querySelector(".player-prompt-modal");
 const aiModal = document.querySelector(".ai-prompt-modal");
-const closeModal = document.querySelector(".close-prompt");
+
+
+let p1Name;
+let p2Name;
+let p1NameAi;
+let aiName;
+const startBtn = document.querySelectorAll(".start-button");
+const closeModal = document.querySelectorAll(".close-prompt");
 
 selectButtonAi.addEventListener("click", aiPrompt);
 selectButtonPlayer.addEventListener("click", playerPrompt);
-closeModal.addEventListener("click", closePrompt);
+
+
+for(let i = 0; i < startBtn.length; i++){
+    startBtn[i].addEventListener("click", startGame);
+};
+
+
+
+for(let i = 0; i < closeModal.length; i++){
+    closeModal[i].addEventListener("click", closePrompt);
+};
+
 
 function aiPrompt(){
     aiModal.showModal();
@@ -41,13 +59,9 @@ function closePrompt(){
     playerModal.close();
 }
 
-let p1Name;
-let p2Name;
-let p1NameAi;
-let aiName;
-const startBtn = document.querySelector(".start-button");
-startBtn.addEventListener("click", startGame);
-// startBtn.addEventListener("click", closePrompt);
+
+
+
 
 function startGame(){
     p1Name = document.querySelector("#p1Name").value;
